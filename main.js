af839769e54f05a99d9727304a5d71c1d1e3f3da
David Zhu <hdavidzhu@gmail.com>
2015-07-11 18:48:16 -0400

    About to Readthedocs.

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-var converter = new showdown.Converter();
+// Add the id tag of the doc as well as the Rawgit to the README.md as well.
+var docs = {
+  "#PearlAPI": "//cdn.rawgit.com/openpearl/PearlAPI/master/README.md",
+  "#PearlClient": "//cdn.rawgit.com/openpearl/PearlClient/master/README.md",
+  "#PearlEngine": "//cdn.rawgit.com/openpearl/PearlEngine/master/README.md"
+}
 
-$.get( "//cdn.rawgit.com/openpearl/PearlAPI/master/README.md", 
-  function(data) {
-    var text = data;
-    var html = converter.makeHtml(text);
-    console.log(html);
-    $('#PearlAPI').html(html);
+// UNDER THE HOOD ********************
+
+var requestREADME = function(doctag, rawgit) {
+  $.ajax({
+    url: rawgit,
+    success: function(data) {
+      var converter = new showdown.Converter();
+      $(doctag).html(converter.makeHtml(data));
+      // Makes links open in a new tab.
+      $(doctag).find("a").attr('target', '_blank');
+    },
+    error: function(error) {
+      console.log(error);
+    }
   });
+}
+
+// Render all READMEs.
+for (doctag in docs) {
+  requestREADME(doctag, docs[doctag]);
+}
